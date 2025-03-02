aed5c3e53ef1695a924e08faf2d6ae04893041d4
kchen <kchen@home.com>
2025-03-02 10:41:56 +0800

    change clang name to llvm

---
+++ README.txt
@@ -0,0 +1,19 @@
+https://docs.conan.io/2/tutorial/developing_packages/local_package_development_flow.html
+
+# Step 1: Prepare sources
+conan source .
+
+# Step 2: Install dependencies
+conan install .
+
+# Step 3: Build
+conan build . profile=../conan_windows_profile.txt
+
+# Step 4: Export to cache
+conan export-pkg .
+
+# Step 5: Check local conan cache
+conan list
+
+# Step 6: Upload to remote repo
+conan upload "*" -r=divineland
--- conan/llvm/conanfile.py
+++ conan/llvm/conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.files import rmdir
 import os
 
 class LlvmConan(ConanFile):
-    name = "clang"
+    name = "llvm"
     version = "19.1.7"
     #license = "<Put the package license here>"
     #author = "<Put your name here> <And your email here>"
@@ -16,6 +16,8 @@ class LlvmConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     default_user = "kplanb"
     default_channel = "default"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
 
     def source(self):
         self.run("git clone https://github.com/llvm/llvm-project.git  --branch llvmorg-%s --depth=1" % (self.version)) 
@@ -27,18 +29,19 @@ class LlvmConan(ConanFile):
         tc = CMakeToolchain(self)
 
         # More configrations could be found in https://llvm.org/docs/CMake.html
-        # tc.preprocessor_definitions["LLVM_ENABLE_PROJECTS"] = "clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;comipler-rt;lld;compiler-rt;polly"
-        tc.preprocessor_definitions["LLVM_ENABLE_PROJECTS"] = "clang;libcxx;libcxxabi;libunwind;lldb;lld;polly"
-        tc.preprocessor_definitions["LLVM_STATIC_LINK_CXX_STDLIB"] = True
+        # tc.cache_variables["LLVM_ENABLE_PROJECTS"] = "bolt;clang;clang-tools-extra;compiler-rt;cross-project-tests;libc;libclc;lld;lldb;mlir;openmp;polly;pstl"
+        tc.cache_variables["LLVM_ENABLE_PROJECTS"] = "bolt;clang;clang-tools-extra;compiler-rt;cross-project-tests;libc;libclc;lld;lldb;mlir;openmp;polly;pstl"
+        tc.cache_variables["LLVM_ENABLE_RUNTIMES"] = "libc;libunwind;libcxxabi;pstl;libcxx;compiler-rt;openmp;llvm-libgcc;offload"
+        tc.cache_variables["LLVM_STATIC_LINK_CXX_STDLIB"] = True
 
-        tc.preprocessor_definitions["LLVM_ENABLE_PIC"] = "ON"
-        tc.preprocessor_definitions["LIBCLANG_BUILD_STATIC"] = "ON"
+        tc.cache_variables["LLVM_ENABLE_PIC"] = "ON"
+        tc.cache_variables["LIBCLANG_BUILD_STATIC"] = "ON"
 
         if self.settings.os == "Windows":
-            tc.preprocessor_definitions["LLVM_USE_CRT_RELEASE"] = self.settings.compiler.runtime
+            tc.cache_variables["LLVM_USE_CRT_RELEASE"] = self.settings.compiler.runtime
         
-        tc.preprocessor_definitions["LLVM_ENABLE_LIBCXX"] = True
-        tc.preprocessor_definitions["CMAKE_INSTALL_CONFIG_NAME"] = self.settings.build_type
+        tc.cache_variables["LLVM_ENABLE_LIBCXX"] = True
+        # tc.cache_variables["CMAKE_INSTALL_CONFIG_NAME"] = self.settings.build_type
 
         tc.cache_variables["CMAKE_USE_OPENSSL"] = "OFF"
         tc.cache_variables["BUILD_TESTING"] = "OFF"
