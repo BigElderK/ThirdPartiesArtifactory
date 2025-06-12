dddff2fb853617d4db2a3ce0dc38daaf96b52ced
BigElderK <BigElderK@divineland.org>
2025-06-12 13:12:39 +0800

    add directx_tex

--- conan/directx_tex/conanfile.py
+++ conan/directx_tex/conanfile.py
@@ -8,65 +8,29 @@ from conan.tools.microsoft import MSBuild
 import os
 
 class FreeImageConan(ConanFile):
-    name = "DirectXTex"
-    version = "sept2023"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    name = "directx_tex"
+    version = "0.4.1"
+    settings = "os", "arch", "build_type"
 
-    def configure(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    package_type = "application"
             
     def layout(self):
         cmake_layout(self, src_folder="./source", build_folder=os.path.join("./build", str(self.settings.os), str(self.settings.arch)))
 
     def source(self):
-        self.run("git clone https://github.com/microsoft/DirectXTex.git --branch %s --depth=1" % (self.version)) 
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        #tc.cache_variables["GLFW_BUILD_WAYLAND"] = False
-        #tc.cache_variables["GLFW_BUILD_X11"] = True
-        tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(build_script_folder="./DirectXTex")
-        cmake.build()
         return
     
-    # def build(self):
-
-    #     if self.settings.os == "Windows":
-    #         msbuild = MSBuild(self)
-    #         if self.options.shared == "True":
-    #             msbuild.build("source/DirectXTex/FreeImage.2017.sln")
-    #         else:
-    #             msbuild.build("FreeImage/Source/FreeImageLib/FreeImageLib.2017.vcxproj")
-                
-    #         #if self.settings.compiler.runtime == 'MT':
-    #         #    cmake.definitions["CMAKE_CXX_FLAGS_RELEASE"] = "/MT"
-    #     else:
-    #         self.run("make --directory=./FreeImage")
+    def build(self):
+        if self.settings.os == "Linux":
+            get(self, url = "https://github.com/matyalatte/Texconv-Custom-DLL/releases/download/v%s/TexconvCustomDLL-v%s-Linux-no-deps.tar.bz2" % (self.version, self.version), destination="bin")
+        if self.settings.os == "Windows":
+            get(self, url = "https://github.com/matyalatte/Texconv-Custom-DLL/releases/download/v%s/TexconvCustomDLL-v%s-Windows.zip" % (self.version, self.version), destination="bin")
+        if self.settings.os == "Macos":
+            get(self, url = "https://github.com/matyalatte/Texconv-Custom-DLL/releases/download/v%s/TexconvCustomDLL-v%s-macOS-no-deps.tar.bz2" % (self.version, self.version), destination="bin")
+        
 
     def package(self):
-        self.copy('*.h', dst='include', src='FreeImage/Dist', keep_path=False)
-        self.copy('*', dst='lib', src='FreeImage/Dist', keep_path=False)
+        copy(self, '*', src=os.path.join(self.build_folder, "bin"), dst=os.path.join(self.package_folder, "bin"))
 
-    def package_info(self):  # still very useful for package consumers
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.libdirs = ['lib']
-        #self.cpp_info.bindirs = ['bin']
-        if self.settings.os == "Windows":
-            if self.options.shared == "True":
-                self.cpp_info.libs = ["FreeImage"]
-            else:
-                self.cpp_info.libs = ["FreeImageLib"]
-                self.cpp_info.defines = ['FREEIMAGE_LIB']
-        else:
-            if self.options.shared == "True":
-                self.cpp_info.libs = ["freeimage-%s" % (self.version)]
-            else:
-                self.cpp_info.libs = ["freeimage"]
-                self.cpp_info.defines = ['FREEIMAGE_LIB']
+    def package_info(self):
+        self.cpp_info.bindirs = ['bin']
