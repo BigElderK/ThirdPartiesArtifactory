c3b7261d8bb7262f25ecf07a4d58cd85ff5f6c10
BigElderK <BigElderK@divineland.org>
2025-07-25 10:49:16 +0800

    add mimalloc

---
+++ conan/mimalloc/conanfile.py
@@ -0,0 +1,59 @@
+import logging
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.scm import Git
+from conan.tools.files import rmdir
+from conan.tools.microsoft import MSBuild
+from conan.tools.files import copy
+import os
+
+class MiMallocConan(ConanFile):
+    name = "mimalloc"
+    version = "3.1.5"
+    settings = "os", "arch", "build_type"
+
+    options = {"fPIC": [True, False], "secure": [True, False]}
+    default_options = {"fPIC": True, "secure": False}
+    
+    # default_user = "bigelderk"
+    # default_channel = "default"
+
+    def layout(self):
+        cmake_layout(self, src_folder="./source", build_folder=os.path.join("./build", str(self.settings.os), str(self.settings.arch)))
+
+    def source(self):
+        self.run("git clone https://github.com/microsoft/mimalloc.git --branch v%s --depth=1" % (self.version)) 
+        
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if self.options.secure == True:
+            tc.cache_variables["MI_SECURE"] = "ON"
+    
+    def build(self):
+        if self.settings.os == "Windows":
+            msbuild = MSBuild(self)
+            msbuild.build(os.path.join(self.source_folder, "mimalloc", "ide", "vs2022", "mimalloc.sln"))
+
+            copy(self, "*", src=os.path.join(self.source_folder, "mimalloc", "out", "msvc-x64", str(self.settings.build_type)), dst=os.path.join(self.build_folder, "lib"), keep_path=True)
+            copy(self, "*", src=os.path.join(self.source_folder, "mimalloc", "include"), dst=os.path.join(self.build_folder, "include"), keep_path=True)
+        else:
+            cmake = CMake(self)
+            cmake.configure(build_script_folder="./mimalloc")
+            cmake.build()
+
+    def package(self):
+        if self.settings.os == "Windows":
+            copy(self, "*", src=os.path.join(self.build_folder, "lib"), dst=os.path.join(self.package_folder, "lib"), keep_path=True)
+            copy(self, "*", src=os.path.join(self.build_folder, "include"), dst=os.path.join(self.package_folder, "include"), keep_path=True)
+        else:
+            cmake = CMake(self)
+            cmake.install()
+
+    def package_info(self):
+        self.cpp_info.includedirs = []
+        self.cpp_info.libdirs = []
+
+        #self.runenv_info.append_path(os.path.join(self.package_folder, "bin"))
+        #self.runenv_info.append_path("PATH", os.path.join(self.package_folder, "bin"))
+        #self.buildenv_info.append_path("PATH", os.path.join(self.package_folder, "bin"))
+        return
