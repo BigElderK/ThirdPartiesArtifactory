4be7d6b71be608b1d0900178781108c225475f2f
BigElderK <BigElderK@divineland.org>
2025-08-16 19:14:57 +0800

    fix mimalloc TLS issue

--- conan/mimalloc/conanfile.py
+++ conan/mimalloc/conanfile.py
@@ -6,6 +6,7 @@ from conan.tools.files import rmdir
 from conan.tools.microsoft import MSBuild
 from conan.tools.files import copy
 import os
+import re
 
 class MiMallocConan(ConanFile):
     name = "mimalloc"
@@ -23,12 +24,40 @@ class MiMallocConan(ConanFile):
 
     def source(self):
         self.run("git clone https://github.com/microsoft/mimalloc.git --branch v%s --depth=1" % (self.version)) 
+
+        # # Read the content of internal.h
+        # patch_head_file = os.path.join(self.source_folder, 'mimalloc', 'include', 'mimalloc', 'internal.h')
+        # with open(patch_head_file, 'r') as file:
+        #     content = file.read()
+
+        # # Define the regex pattern to match the line (allowing variable whitespace)
+        # pattern = r'^\s*#define\s+mi_decl_thread\s+__thread\s*$'
+        # replacement = '#define mi_decl_thread          __attribute__((visibility("default"))) __attribute__((aligned(64))) __thread'
+        # # Perform the replacement using regex (multiline mode)
+        # content = re.sub(pattern, replacement, content, flags=re.MULTILINE)
+
+        # # Define the regex pattern to match the line (allowing variable whitespace)
+        # pattern = r'#define mi_decl_hidden\s+__attribute__\(\(visibility\("hidden"\)\)\)'
+        # replacement = '#define mi_decl_hidden'
+        # # Perform the replacement using regex (multiline mode)
+        # content = re.sub(pattern, replacement, content, flags=re.MULTILINE)
+
+        # # Write the modified content back to internal.h
+        # with open(patch_head_file, 'w') as file:
+        #     file.write(content)
         
     def generate(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["MI_BUILD_STATIC"] = "ON"
+        tc.cache_variables["MI_OVERRIDE"] = "OFF"
+        tc.cache_variables["MI_LOCAL_DYNAMIC_TLS"] = "ON"
+        
         if self.options.secure == True:
             tc.cache_variables["MI_SECURE"] = "ON"
+
+        if self.settings.os != "Windows":
+            tc.cache_variables["CMAKE_C_FLAGS"] = "-ftls-model=global-dynamic"
+            tc.cache_variables["CMAKE_CXX_FLAGS"] = "-ftls-model=global-dynamic"
         tc.generate()
     
     def build(self):
